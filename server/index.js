8bec8b0daab008ce4bf83c20ce58cfddd4314c51
lekhan7 <ktkarumbaiah@gmail.com>
2026-03-21 20:53:52 +0530

    Lekhan

--- server/index.js
+++ server/index.js
@@ -12,14 +12,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://exam-hub2.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://exam-hub2.vercel.app/"],
     methods: ["GET", "POST"]
   }
 });
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "https://exam-hub2.vercel.app"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://exam-hub2.vercel.app/"],
   methods: ["GET", "POST"]
 }));
 app.use(express.json());
