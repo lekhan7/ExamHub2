b7e2ea02fa212ba38e26f7de9237e3c70237ea83
lekhan7 <ktkarumbaiah@gmail.com>
2026-03-21 20:20:44 +0530

    Lekhan Full Last commit

--- server/index.js
+++ server/index.js
@@ -12,13 +12,16 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174",'https://exam-hub2.vercel.app/'],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://exam-hub2.vercel.app"],
     methods: ["GET", "POST"]
   }
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://exam-hub2.vercel.app"],
+  methods: ["GET", "POST"]
+}));
 app.use(express.json());
 
 // Configure multer for PDF uploads (memory storage)
